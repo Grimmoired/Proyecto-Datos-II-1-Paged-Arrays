bd24306839b5bdf968f3d11f47c9867c1c137a46
Bryan Abarca <bryandavidabarcaquiros@gmail.com>
2026-03-09 15:52:56 -0600

    Creacion e implementacion de la clase pagedArray.

--- PagedArray.cpp
+++ PagedArray.cpp
@@ -3,3 +3,79 @@
 //
 
 #include "PagedArray.h"
+
+PagedArray::PagedArray(const char *filePath, int pageSize, int pageCount) {
+    this-> pageSize = pageSize;
+    this-> pageCount = pageCount;
+    this-> filePath = filePath;
+    faultCount = 0;
+    hitCount = 0;
+    indexFIFO = 0;
+
+    ptrPageFile = fopen(filePath, "r+b");
+    if (ptrPageFile == nullptr) {
+        std::cerr << "Error: no se pudo abrir el archivo, path inexistente" << filePath << std::endl;
+        exit(1);
+    }
+
+    pages = new int*[pageCount];
+    pageNumArray = new int[pageCount];
+    fseek(ptrPageFile, 0, SEEK_END);
+    long sizeEnBytes = ftell(ptrPageFile);
+    this-> arraySize = sizeEnBytes / 4;
+    fseek(ptrPageFile, 0, SEEK_SET);
+
+    for (int i = 0; i < pageCount; i++) {
+        pages[i] = nullptr;
+        pageNumArray[i] = -1;
+    }
+}
+
+
+PagedArray::~PagedArray() {
+    for (int i = 0; i < pageCount; i++) {
+        if (pages[i] != nullptr) {
+            long posicion = (long)pageNumArray[i] * 4 * pageSize;
+            fseek(ptrPageFile, posicion, SEEK_SET);
+            fwrite(pages[i], sizeof(int), pageSize, ptrPageFile);
+            delete[] pages[i];
+            pages[i] = nullptr;
+        }
+    }
+    delete[] pages;
+    delete[] pageNumArray;
+    fclose(ptrPageFile);
+}
+
+int& PagedArray::operator[](int index) {
+    int pageNum = index / pageSize;
+    int offset = index % pageSize;
+
+
+    for (int i = 0; i < pageCount; i++) {
+        if (pageNumArray[i] == pageNum) {
+            hitCount++;
+            return pages[i][offset];
+        }
+    }
+
+    faultCount++;
+
+    if (pages[indexFIFO] != nullptr) {
+        long posicion = (long)pageNumArray[indexFIFO] * 4 * pageSize;
+        fseek(ptrPageFile, posicion, SEEK_SET);
+        fwrite(pages[indexFIFO], sizeof(int), pageSize, ptrPageFile);
+        delete[] pages[indexFIFO];
+        pages[indexFIFO] = nullptr;
+    }
+
+    pages[indexFIFO] = new int[pageSize];
+    pageNumArray[indexFIFO] = pageNum;
+    long posicion = (long)pageNumArray[indexFIFO] * 4 * pageSize;
+    fseek(ptrPageFile, posicion, SEEK_SET);
+    fread(pages[indexFIFO], sizeof(int), pageSize, ptrPageFile);
+    int espacioUsado = indexFIFO;
+    indexFIFO = (indexFIFO + 1) % pageCount;
+    return pages[espacioUsado][offset];
+
+}
--- PagedArray.h
+++ PagedArray.h
@@ -4,10 +4,27 @@
 
 #ifndef PROYECTO_DATOS_II_1_PAGED_ARRAYS_PAGEDARRAY_H
 #define PROYECTO_DATOS_II_1_PAGED_ARRAYS_PAGEDARRAY_H
+#include <iostream>
+#include <cstdio>
 
 
 class PagedArray {
-};
+private:
+    int** pages;
+    FILE* ptrPageFile;
+    int pageSize;
+    int pageCount;
+    int faultCount;
+    int hitCount;
+    int* pageNumArray;
+    int arraySize;
+    const char* filePath;
+    int indexFIFO;
 
+public:
+    PagedArray(const char* filePath, int pageSize, int pageCount);
+    ~PagedArray();
+    int& operator[](int index);
+};
 
 #endif //PROYECTO_DATOS_II_1_PAGED_ARRAYS_PAGEDARRAY_H
